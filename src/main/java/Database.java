d268a653c052e5893c8b3f09ac842ca52be3b497
avani987 <avani.9khurana@gmail.com>
2017-07-12 13:33:29 +0530

    Initial commit

---
+++ src/main/java/Database.java
@@ -0,0 +1,25 @@
+public class Database {
+
+    int port;
+    String Name;
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+
+
+}
+
